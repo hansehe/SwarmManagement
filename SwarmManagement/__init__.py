f319a90ae7c05d32fecd3b676ced0e73ed1d8f8f
Hans Erik Heggem <hanserik@neate.no>
2025-05-13 17:24:21 +0200

    Fixed log info by default

--- SwarmManagement/__init__.py
+++ SwarmManagement/__init__.py
@@ -6,8 +6,8 @@ import logging
 def main():
     """Entry point for the application script"""
     arguments = sys.argv[1:]
+    logging.basicConfig(level=logging.INFO)
     SwarmManager.HandleManagement(arguments)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     main()
